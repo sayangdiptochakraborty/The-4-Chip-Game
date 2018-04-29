7d7b8ea68234525d299ebc060e7fb266ed1c955c
Sayangdipto Chakraborty <sayangdipto.chakraborty556@gmail.com>
2018-04-29 19:11:37 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,9 @@
+function promptMe()
+{
+    var a=prompt("Enter name of player one, you will be Blue.");
+    alert("Hello "+a+"!");
+    var b=prompt("Hello again, enter name of player two, you will be red");
+    alert("Hello "+b+"!");
+    
+    
+}
